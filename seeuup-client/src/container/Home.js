8d2af84eaa5e63c36ab9416981a4c78abc630125
Mackenson <mackensondorancy@yahoo.com>
2018-10-23 22:49:48 -0400

    updated

---
+++ seeuup-client/src/container/Home.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import Registration from './Registration'
+
+class Home extends Component {
+  state = {
+    response: ''
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/home');
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Welcome to SeeUUp</h1>
+        </header>
+        <p>{this.state.response}</p>
+        <Registration />
+      </div>
+    );
+  }
+}
+
+export default Home;
